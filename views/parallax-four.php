9ab3f8f99a0ce560909d90b2e4266369286e7bfc
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2018-05-23 11:08:50 -0300

    improvement: (Tickets) Adicionando frame da sympla para compra de ingressos

--- views/parallax-four.php
+++ views/parallax-four.php
@@ -3,7 +3,11 @@
       <div class="container">
         <div class="row center">
           <h2 class="header col s12 light text-parallax"><strong>INSCREVA-SE!</strong></h2>
-          <a target="_blank" href="http://bit.ly/rapaduraconf" class="waves-effect waves-light btn-large color-marron">Clique aqui para comprar o seu ingresso</a>
+
+          <div id="sympla-widget-288555" height="auto"></div>
+          <script src="https://www.sympla.com.br/js/sympla.widget-pt.js/288555"></script>
+
+          <!--<a target="_blank" href="http://bit.ly/rapaduraconf" class="waves-effect waves-light btn-large color-marron">Clique aqui para comprar o seu ingresso</a>-->
         </div>
       </div>
     </div>
