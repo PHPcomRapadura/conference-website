c4243a329907d6a907f15cdca8fd3b70d9db1186
Kilderson Sena <dersonsena@gmail.com>
2018-05-25 22:20:22 -0300

    adicionando gitignore, editorconfig e correcoes de layout

--- views/banner.php
+++ views/banner.php
@@ -1,24 +1,24 @@
 <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot" id="home">
-      <div class="container">
-    
-        <div class="row center">
-          <img id="img-banner" class="responsive-img" src="assets/img/logo-rapadura.png" alt="PHP com Rapadura">
+        <div class="container">
+            <div class="row center">
+                <img id="img-banner" class="responsive-img" src="assets/img/logo-rapadura.png" alt="PHP com Rapadura">
+            </div>
+            <div class="row center logo-conference">
+                <img id="img-banner" class="responsive-img" src="assets/img/logo-conference-pcr-brown.png" alt="PHP com Rapadura CONFERENCE">
+            </div>
+            <div class="row center">
+                <h5 class="header col s12 light font-marron"><strong>09 de Junho de 2018</strong></h5>
+                <h5 class="header col s12 light font-marron"><strong>UNIFOR - Universidade de Fortaleza</strong></h5>
+            </div>
+
+            <div class="row center">
+                <a href="#about" id="download-button"
+                   class="btn-large waves-effect waves-light color-marron scrollSuave">SAIBA MAIS</a>
+            </div>
         </div>
-        <div class="row center">
-          <img id="img-banner" class="responsive-img" src="assets/img/logo-conference-pcr-brown.png" alt="PHP com Rapadura CONFERENCE">
-        </div>
-        <div class="row center">
-          <h5 class="header col s12 light font-marron"><strong>09 de Junho de 2018</strong></h5>
-          <h5 class="header col s12 light font-marron"><strong>UNIFOR - Universidade de Fortaleza</strong></h5>
-        </div>
-       
-        <div class="row center">
-          <a href="#about" id="download-button" 
-          class="btn-large waves-effect waves-light color-marron scrollSuave">SAIBA MAIS</a>
-        </div>
-        
-      </div>
     </div>
-    <div class="parallax"><img src="/assets/img/banner-site.png" alt="PHP com Rapadura CONFERENCE 2018"></div>
-  </div>
+    <div class="parallax">
+        <img src="/assets/img/banner-site.png" alt="PHP com Rapadura CONFERENCE 2018" />
+    </div>
+</div>
--- views/base.php
+++ views/base.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <link rel="icon" href="assets/img/favicon.png">
-  <title>PHP com Rapadura CONFERENCE 2018</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="icon" href="assets/img/favicon.png">
+    <title>PHP com Rapadura CONFERENCE 2018</title>
 
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <script src="https://use.fontawesome.com/6c531641d9.js"></script>
-  <link href="assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://use.fontawesome.com/6c531641d9.js"></script>
+    <link href="assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body class="amber lighten-4">
-  
-   <!-- Top Navigation -->
-   <?php require_once 'views/nav.php'; ?>
 
-   <!-- Section Banner -->
-   <?php require_once 'views/banner.php'; ?>
+    <!-- Top Navigation -->
+    <?php require_once 'views/nav.php'; ?>
 
-   <!-- Section Parallax One -->
-   <?php require_once 'views/parallax-one.php'; ?>
+    <!-- Section Banner -->
+    <?php require_once 'views/banner.php'; ?>
 
-   <!-- Section About -->
-   <?php require_once 'views/about.php'; ?>
+    <!-- Section Parallax One -->
+    <?php require_once 'views/parallax-one.php'; ?>
+
+    <!-- Section About -->
+    <?php require_once 'views/about.php'; ?>
 
     <!-- Section Speakers -->
     <?php require_once 'views/speakers.php'; ?>
@@ -47,22 +47,22 @@
     <!-- Section sponsors -->
     <?php require_once 'views/sponsors.php'; ?>
 
-     <!-- Section sponsors -->
-     <?php require_once 'views/footer.php'; ?>
+    <!-- Section sponsors -->
+    <?php require_once 'views/footer.php'; ?>
 
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="assets/js/materialize.js"></script>
-  <script src="assets/js/init.js"></script>
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="assets/js/materialize.js"></script>
+<script src="assets/js/init.js"></script>
 
-  </body>
+</body>
 </html>
 <script>
-var $doc = $('html, body');
-$('.scrollSuave').click(function() {
-    $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 2000);
-    return false;
-});
+    var $doc = $('html, body');
+    $('.scrollSuave').click(function() {
+        $doc.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 2000);
+        return false;
+    });
 </script>
