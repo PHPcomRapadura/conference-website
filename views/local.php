a475e455976975f3c7c948c26ef432de38c6c491
Kilderson Sena <dersonsena@gmail.com>
2019-10-22 10:40:32 -0300

    secao do local do evento alterado

--- views/local.php
+++ views/local.php
@@ -1,30 +1,26 @@
 <div class="container">
-     <div class="section" id="local">
+    <div class="section" id="local">
         <div class="row">
-
             <div class="col s12 center">
                 <h3 class="font-marron"><strong>Local</strong></h3>
-                <h5 class="font-marron">Veja em detalhes onde será o evento</h5> 
+                <h5 class="font-marron">Veja em detalhes onde será o evento</h5>
             </div>
-
-              <div class="col s12 m8">
+            <div class="col s12 m8">
                 <div class="icon-block">
-                  <br><br>
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15924.768019710125!2d-38.4784229!3d-3.7683568!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd435fd5a6bfbbe44!2sUNIFOR+-+Universidade+de+Fortaleza!5e0!3m2!1spt-BR!2sbr!4v1526845017300" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <br><br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15925.390332789862!2d-38.4931569!3d-3.7342083!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x85fe0c6f813ffbdc!2sCentro%20Universit%C3%A1rio%20Christus%20-%20Unichristus!5e0!3m2!1spt-BR!2sbr!4v1571751556155!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                 </div>
-              </div>
-
-              <div class="col s12 m4">
+            </div>
+            <div class="col s12 m4">
                 <div class="icon-block">
-                    <H1><br></H1>
-                    <h4 class="font-marron"><strong>UNIFOR</strong></h4> 
-                    <h5 class="font-marron">Universidade de Fortaleza</h5>
+                    <h1><br></h1>
+                    <h4 class="font-marron"><strong>UniChristus</strong></h4>
+                    <h5 class="font-marron">Centro Universitário Christus</h5>
                     <p class="light font-marron">
-                        Av. Washington Soares, 1321 - Edson Queiroz, Fortaleza - Ceará.
+                        Av. Dom Luís, 911 - Meireles, Fortaleza - CE, 60160-230
                     </p>
                 </div>
-              </div>
-
+            </div>
         </div>
     </div>
 </div>
