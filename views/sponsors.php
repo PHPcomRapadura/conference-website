9393d1bd14640c439a21b5cb0cf09ee255082c32
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2019-11-01 10:27:41 -0300

    Add Cyrille sponsor

--- views/sponsors.php
+++ views/sponsors.php
@@ -15,12 +15,34 @@
                     <div class="card-content white-text">
                         <span class="card-title font-marron"><strong>Patrocínio Rapadura Preta</strong></span>
                         <hr />
-                        <p class="font-marron">Saiba como a sua empresa pode ajudar-nos a realizar um evento fantástico.</p>
-                    </div>
-                    <div class="card-action font-marron">
-                        <a class="waves-effect waves-light btn color-marron" target="_blank" href="http://conference.phpcomrapadura.org/assets/files/midia-kit-conference.pdf">
-                            <i class="fa fa-plus-circle"></i> Saiba mais
-                        </a>
+                        <br>
+                        <div class="row">
+                            <div class="col m5 offset-m1">
+                                <img class="responsive-img" width="70%" id="img-sponsors" src="assets/img/sponsors/cyrille.jpg" alt="Cyrille Grandval" title="Cyrille Grandval">
+                            </div>
+                            <div class="col m5">
+                                <h4 class="h2 brown-text">Cyrille Grandval</h4>
+                                <hr>
+                                <p class="brown-text" style="font-size: 14pt">
+                                    Fundador da <a href="http://darkmira.fr">Darkmira e Darkmira Brasil</a>; Organizador e Idealizador do <a href="http://darkmiratour.rocks">Darkmira Tour PHP</a>;
+                                    Organizador do Symfony Live Brasil; 
+                                    Professor e coordenador de cursos em Paris;  Empreendedor e Evangelizador de PHP, Segurança e Qualidade.
+                                </p>
+
+                                <br>
+                                <a class="brown-text text-darken-4" title="Twitter" target="_blank" href="https://twitter.com/CyrilleGrandval/"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+                                </a>
+                                &nbsp;
+                                &nbsp;
+                                <a class="brown-text text-darken-4" title="instagram" target="_blank" href="https://www.instagram.com/cyrillegrandval/"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                                </a>
+
+                                &nbsp;
+                                &nbsp;
+                                <a class="brown-text text-darken-4" title="github" target="_blank" href="http://github.com/cgrandval/"><i class="fa fa-github fa-3x" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
