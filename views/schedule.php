aa65710d6062d8ed4e513abb15cc1476d998c5a2
Luítame de Oliveira <luitame.oliveira@petcoach.co>
2019-11-14 10:17:03 -0300

    programacao final do evento

--- views/schedule.php
+++ views/schedule.php
@@ -15,67 +15,67 @@
                         </thead>
                         <tbody>
                             <tr>
-                                <td>07:30 - 08:00</td>
-                                <td>Café e Credenciamento</td>
-                                <td>Comunidade PHP com Rapadura</td>
-                            </tr>
-                            <tr>
-                                <td>08:20 - 08:40</td>
+                                <td>08:00</td>
                                 <td>Abertura</td>
-                                <td>Comunidade PHP com Rapadura e Unichristus</td>
+                                <td>Abertura</td>
                             </tr>
                             <tr>
-                                <td>08:45 - 09:25</td>
-                                <td>Trabalhando com Value Objects com PHP</td>
-                                <td>Ian Rodrigues</td>
-                            </tr>
-                            <tr>
-                                <td>09:30 - 10:10</td>
-                                <td>Vixe tem bug... e agora?</td>
-                                <td>Juliana Martins</td>
-                            </tr>
-                            <tr>
-                                <td>10:15 - 10:55</td>
-                                <td>Eventos, PHP e Microsserviços, pode dar em festa, até via streaming.</td>
-                                <td>Chicão Thiago</td>
-                            </tr>
-                            <tr>
-                                <td>11:00 - 11:40</td>
-                                <td>PHP Internals: Primeiros Passos</td>
-                                <td>Adiel Cristo</td>
-                            </tr>
-                            <tr>
-                                <td>11:40 - 13:30</td>
-                                <td>Intervalo para almoço</td>
-                                <td>Todo mundo!</td>
-                            </tr>
-                            <tr>
-                                <td>13:35 - 14:05</td>
-                                <td>Construindo estruturas complexas com serialização de obejtos</td>
-                                <td>Alef Castelo</td>
-                            </tr>
-                            <tr>
-                                <td>14:10 - 14:50</td>
+                                <td>08:20</td>
                                 <td>Testes Gray Box - Aplicação do melhor dos dois mundos</td>
                                 <td>Carol Rodrigues</td>
                             </tr>
                             <tr>
-                                <td>14:50 - 15:30</td>
+                                <td>09:10</td>
+                                <td>Eventos, PHP e Microsserviços, pode dar em festa, até via streaming.</td>
+                                <td>Chicão Thiago</td>
+                            </tr>
+                            <tr>
+                                <td>09:50</td>
                                 <td>Intervalo para coffee break</td>
                                 <td>Comunidade PHP com Rapadura</td>
                             </tr>
                             <tr>
-                                <td>15:35 - 16:05</td>
-                                <td>Aplicações PHP Serverless</td>
-                                <td>Luítame de Oliveira</td>
+                                <td>10:20</td>
+                                <td>DDD, pra que lado vai Isso?!</td>
+                                <td>Adiel Cristo</td>
                             </tr>
                             <tr>
-                                <td>16:10 - 16:50</td>
+                                <td>11:10</td>
+                                <td>Vixe tem bug... e agora?</td>
+                                <td>Juliana Martins</td>
+                            </tr>
+                            <tr>
+                                <td>12:00</td>
+                                <td>Intervalo para almoço</td>
+                                <td>Todo mundo!</td>
+                            </tr>
+                            <tr>
+                                <td>13:00</td>
                                 <td>Então me ajude a atualizar, PHP7 eu quero é você</td>
                                 <td>Raquel Barra</td>
                             </tr>
                             <tr>
-                                <td>17:00 - 17:30</td>
+                                <td>13:50</td>
+                                <td>Aplicações PHP Serverless</td>
+                                <td>Luítame de Oliveira</td>
+                            </tr>
+                            <tr>
+                                <td>14:40</td>
+                                <td>PHPizza</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>15:20</td>
+                                <td>Trabalhando com Value Objects com PHP</td>
+                                <td>Ian Rodrigues</td>
+                            </tr>
+                            <tr>
+                                <td>16:10</td>
+                                <td>Kahoot</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>16:40</td>
                                 <td>Encerramento: Agradecimentos e sorteio de brindes</td>
                                 <td>Comunidade PHP com Rapadura</td>
                             </tr>
